f3406dd79c831fa0cdee0e03c1d0dbf6cd344341
JM_Medina <juamedrod@hotmail.es>
2021-10-21 00:03:01 +0200

    JS View of active tasks complete

--- js/view.js
+++ js/view.js
@@ -21,15 +21,79 @@ btnAddTask.addEventListener('click', toggleAddMenu);
 
 function paintTasks(tasks, section) {
     section.innerHTML = '';
-
     tasks.forEach(task => {
-        pintarTask(task, section);
+        paintTask(task, section);
     });
 }
 
 
-function pintarTask(task, section) {
+function paintTask(task, section) {
+    let article = document.createElement('article');
+    let divTask = document.createElement('div');
+    let divDescription = document.createElement('div');
+    let circle = document.createElement('i');
+    let reciclebin = document.createElement('i');
+    let h3 = document.createElement('h3');
+    let pPercent = document.createElement('p');
+    let pDescription = document.createElement('p');
+    let inputPercent = document.createElement('input');
+    let btnComplete = document.createElement('button');
 
+    h3.innerText = task.title;
+    pPercent.innerText = task.percent + '%';
+    pDescription.innerText = task.description;
+    divTask.classList.add('task');
+    circle.classList.add('fas');
+    circle.classList.add('fa-circle');
+    reciclebin.className = 'fas fa-trash-alt';
+    reciclebin.dataset.taskid = task.id;
+    divDescription.classList.add('description');
+    inputPercent.type = 'range';
+    inputPercent.dataset.taskid = task.id;//ojo que esto es un numero
+    inputPercent.id = 'percent';
+    btnComplete.dataset.taskid = task.id;
+    btnComplete.innerText = 'COMPLETE';
+
+    inputPercent.addEventListener('click', updatePercent);
+    reciclebin.addEventListener('click', deleteTask);
+    circle.addEventListener('click', rotatePriority);
+    h3.addEventListener('click', showDescription);
+
+    divTask.appendChild(circle);
+    divTask.appendChild(h3);
+    divTask.appendChild(pPercent);
+    divTask.appendChild(reciclebin);
+
+    divDescription.appendChild(pDescription);
+    divDescription.appendChild(inputPercent);
+    divDescription.appendChild(btnComplete);
+    article.appendChild(divTask);
+    article.appendChild(divDescription);
+    section.appendChild(article);
+
+
+}
+
+
+
+function rotatePriority(event) {
+    console.log('hello3');
+}
+
+function updatePercent(event) {
+    console.log('hello1');
+}
+
+function deleteTask(event) {
+    console.log('hello2');
+}
+
+
+
+
+
+function showDescription(event) {
+    console.log('hello4');
 }
 
 
@@ -39,16 +103,7 @@ function pintarTask(task, section) {
 
 
 
-
-
-
-
-
-
-
-
-
-
+paintTasks(tasksActive, seccionTasks);//para test, quitar despues!!!
 
 
 
