c06faf978e0226ba9f950410e2abe1c25aa6ceb6
JM_Medina <juamedrod@hotmail.es>
2021-10-20 23:19:09 +0200

    e

---
+++ js/model.js
@@ -0,0 +1,15 @@
+const tasksActive = new Array(
+    {
+        id: 1,
+        title: 'Estudia JS o te rajo',
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nemo tempore explicabo quo in perferendis ut, asperiores sint. Fuga, corrupti?",
+        percent: 0,
+    },
+    {
+        id: 2,
+        title: 'Estudia JS o te rajo',
+        description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nemo tempore explicabo quo in perferendis ut, asperiores sint. Fuga, corrupti?",
+        percent: 98,
+    },
+
+);
---
+++ js/view.js
@@ -0,0 +1,74 @@
+/*HTML ELEMENT CAPTION*/
+const seccionTasks = document.querySelector('.tasks');
+const btnAddTask = document.querySelector('#addTask');
+const seccionPopUp = document.querySelector('.popup');
+
+
+/*Global Variables*/
+let isMenuOpen = false;
+let newId = () => taskToDo.at(-1).id + 1;
+
+
+/*********************/
+
+btnAddTask.addEventListener('click', toggleAddMenu);
+
+
+
+
+
+
+
+function paintTasks(tasks, section) {
+    section.innerHTML = '';
+
+    tasks.forEach(task => {
+        pintarTask(task, section);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function toggleAddMenu(event) {
+    if (isMenuOpen) {
+        seccionTasks.style.display = '';
+        seccionPopUp.style.display = 'none';
+    } else {
+        seccionTasks.style.display = 'none';
+        seccionPopUp.style.display = 'flex';
+    }
+    isMenuOpen = !isMenuOpen;
+}
+
